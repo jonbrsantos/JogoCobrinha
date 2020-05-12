5d05530b4d353f76bdcd127b95753722baf3bf0c
Jonathan Santos <jonbrsantos@gmail.com>
2020-05-12 00:59:23 -0300

    Jogo Completo

--- script.js
+++ script.js
@@ -45,6 +45,13 @@ function iniciarJogo(){
     if (snake[0].x < 0 * box && direction == "left") snake[0].x = 15 * box;
     if (snake[0].y < 0 * box && direction == "up") snake[0].y = 15 * box;
 
+    for(i = 1 ; i < snake.length ; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('GameOver');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     criarFrutinha();
@@ -57,7 +64,12 @@ function iniciarJogo(){
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    } else{
+        food.x = Math.floor(Math.random() * 15+1)*box;
+        food.y = Math.floor(Math.random() * 15+1)*box;
+    }
 
     let newHead = {
         x: snakeX,
