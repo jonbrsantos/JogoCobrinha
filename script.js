8c8467a232fbf6d778532e62b9df8e039d846245
Jonathan Santos <jonbrsantos@gmail.com>
2020-05-12 00:37:53 -0300

    Movimento dentro do canvas definido

--- script.js
+++ script.js
@@ -20,7 +20,22 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if (event.keyCode == 37 && direction !="right") direction = "left";
+    if (event.keyCode == 38 && direction !="down") direction = "up";
+    if (event.keyCode == 39 && direction !="left") direction = "right";
+    if (event.keyCode == 40 && direction !="up") direction = "down";
+}
+
 function iniciarJogo(){
+
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].x < 0 * box && direction == "left") snake[0].x = 15 * box;
+    if (snake[0].y < 0 * box && direction == "up") snake[0].y = 15 * box;
+
     criarBG();
     criarCobrinha();
 
